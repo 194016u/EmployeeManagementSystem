308c2e2b717fc5e3bec555f67bddc8b46abe94b7
VIRANGA BANDARA <virangabandara97@gmail.com>
2023-03-04 23:17:51 +0530

    use moddlemapper

--- src/main/java/com/example/emplyeemanagementsystem/repo/EmployeeRepo.java
+++
@@ -1,4 +0,0 @@
-package com.example.emplyeemanagementsystem.repo;
-
-public interface EmployeeRepo {
-}
--- src/main/java/com/example/emplyeemanagementsystem/service/EmployeeService.java
+++
@@ -1,4 +0,0 @@
-package com.example.emplyeemanagementsystem.service;
-
-public class EmployeeService {
-}
